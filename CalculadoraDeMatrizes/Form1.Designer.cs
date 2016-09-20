41d61680fd7c594ff650af8a43d7d3c73d0dfa51
DavidMedeiros <davidmedeiros.nave@gmail.com>
2016-09-20 10:57:40 -0300

    Calculadora De Matrizes
    Duas matrizes e matriz resultado.

--- CalculadoraDeMatrizes/Form1.Designer.cs
+++ CalculadoraDeMatrizes/Form1.Designer.cs
@@ -31,14 +31,36 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.ButtonAplicar1 = new System.Windows.Forms.Button();
+            this.LabelLeiDeFormação1 = new System.Windows.Forms.Label();
+            this.LabelMatriz1 = new System.Windows.Forms.Label();
             this.ButtonCriarMatriz1 = new System.Windows.Forms.Button();
-            this.ColunasMatriz1 = new System.Windows.Forms.TextBox();
-            this.LinhasMatriz1 = new System.Windows.Forms.TextBox();
             this.groupBoxMatriz1 = new System.Windows.Forms.GroupBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.LabelMatriz1 = new System.Windows.Forms.Label();
+            this.PanelMatriz1 = new System.Windows.Forms.Panel();
+            this.LinhasMatriz1 = new System.Windows.Forms.NumericUpDown();
+            this.ColunasMatriz1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBoxMatriz2 = new System.Windows.Forms.GroupBox();
+            this.PanelMatriz2 = new System.Windows.Forms.Panel();
+            this.ButtonCriarMatriz2 = new System.Windows.Forms.Button();
+            this.LabelMatriz2 = new System.Windows.Forms.Label();
+            this.LabelLeiDeFormação2 = new System.Windows.Forms.Label();
+            this.ButtonAplicar2 = new System.Windows.Forms.Button();
+            this.LinhasMatriz2 = new System.Windows.Forms.NumericUpDown();
+            this.ColunasMatriz2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBoxMatrizResultante = new System.Windows.Forms.GroupBox();
+            this.PanelMatrizResultante = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.groupBoxMatriz1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LinhasMatriz1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColunasMatriz1)).BeginInit();
+            this.groupBoxMatriz2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LinhasMatriz2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColunasMatriz2)).BeginInit();
+            this.groupBoxMatrizResultante.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -48,27 +70,66 @@
             this.tabControl1.Location = new System.Drawing.Point(1, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(825, 462);
+            this.tabControl1.Size = new System.Drawing.Size(990, 462);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.LabelMatriz1);
-            this.tabPage1.Controls.Add(this.ButtonCriarMatriz1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.ColunasMatriz2);
             this.tabPage1.Controls.Add(this.ColunasMatriz1);
+            this.tabPage1.Controls.Add(this.LinhasMatriz2);
             this.tabPage1.Controls.Add(this.LinhasMatriz1);
+            this.tabPage1.Controls.Add(this.ButtonAplicar2);
+            this.tabPage1.Controls.Add(this.ButtonAplicar1);
+            this.tabPage1.Controls.Add(this.LabelLeiDeFormação2);
+            this.tabPage1.Controls.Add(this.LabelMatriz2);
+            this.tabPage1.Controls.Add(this.LabelLeiDeFormação1);
+            this.tabPage1.Controls.Add(this.ButtonCriarMatriz2);
+            this.tabPage1.Controls.Add(this.groupBoxMatrizResultante);
+            this.tabPage1.Controls.Add(this.LabelMatriz1);
+            this.tabPage1.Controls.Add(this.groupBoxMatriz2);
+            this.tabPage1.Controls.Add(this.ButtonCriarMatriz1);
             this.tabPage1.Controls.Add(this.groupBoxMatriz1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(817, 436);
+            this.tabPage1.Size = new System.Drawing.Size(982, 436);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Matriz";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonAplicar1
+            // 
+            this.ButtonAplicar1.Location = new System.Drawing.Point(201, 55);
+            this.ButtonAplicar1.Name = "ButtonAplicar1";
+            this.ButtonAplicar1.Size = new System.Drawing.Size(74, 23);
+            this.ButtonAplicar1.TabIndex = 7;
+            this.ButtonAplicar1.Text = "Aplicar";
+            this.ButtonAplicar1.UseVisualStyleBackColor = true;
+            // 
+            // LabelLeiDeFormação1
+            // 
+            this.LabelLeiDeFormação1.AutoSize = true;
+            this.LabelLeiDeFormação1.Location = new System.Drawing.Point(3, 55);
+            this.LabelLeiDeFormação1.Name = "LabelLeiDeFormação1";
+            this.LabelLeiDeFormação1.Size = new System.Drawing.Size(86, 13);
+            this.LabelLeiDeFormação1.TabIndex = 5;
+            this.LabelLeiDeFormação1.Text = "Lei de Formação";
+            // 
+            // LabelMatriz1
+            // 
+            this.LabelMatriz1.AutoSize = true;
+            this.LabelMatriz1.Location = new System.Drawing.Point(4, 20);
+            this.LabelMatriz1.Name = "LabelMatriz1";
+            this.LabelMatriz1.Size = new System.Drawing.Size(44, 13);
+            this.LabelMatriz1.TabIndex = 4;
+            this.LabelMatriz1.Text = "Matriz 1";
             // 
             // ButtonCriarMatriz1
             // 
-            this.ButtonCriarMatriz1.Location = new System.Drawing.Point(93, 56);
+            this.ButtonCriarMatriz1.Location = new System.Drawing.Point(138, 17);
             this.ButtonCriarMatriz1.Name = "ButtonCriarMatriz1";
             this.ButtonCriarMatriz1.Size = new System.Drawing.Size(75, 23);
             this.ButtonCriarMatriz1.TabIndex = 3;
@@ -76,25 +137,12 @@
             this.ButtonCriarMatriz1.UseVisualStyleBackColor = true;
             this.ButtonCriarMatriz1.Click += new System.EventHandler(this.ButtonCriarMatriz1_Click);
             // 
-            // ColunasMatriz1
-            // 
-            this.ColunasMatriz1.Location = new System.Drawing.Point(163, 17);
-            this.ColunasMatriz1.Name = "ColunasMatriz1";
-            this.ColunasMatriz1.Size = new System.Drawing.Size(100, 20);
-            this.ColunasMatriz1.TabIndex = 1;
-            // 
-            // LinhasMatriz1
-            // 
-            this.LinhasMatriz1.Location = new System.Drawing.Point(57, 17);
-            this.LinhasMatriz1.Name = "LinhasMatriz1";
-            this.LinhasMatriz1.Size = new System.Drawing.Size(100, 20);
-            this.LinhasMatriz1.TabIndex = 1;
-            // 
             // groupBoxMatriz1
             // 
-            this.groupBoxMatriz1.Location = new System.Drawing.Point(7, 116);
+            this.groupBoxMatriz1.Controls.Add(this.PanelMatriz1);
+            this.groupBoxMatriz1.Location = new System.Drawing.Point(7, 84);
             this.groupBoxMatriz1.Name = "groupBoxMatriz1";
-            this.groupBoxMatriz1.Size = new System.Drawing.Size(370, 270);
+            this.groupBoxMatriz1.Size = new System.Drawing.Size(304, 245);
             this.groupBoxMatriz1.TabIndex = 0;
             this.groupBoxMatriz1.TabStop = false;
             this.groupBoxMatriz1.Text = "Matriz 1";
@@ -109,20 +157,136 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // LabelMatriz1
+            // PanelMatriz1
             // 
-            this.LabelMatriz1.AutoSize = true;
-            this.LabelMatriz1.Location = new System.Drawing.Point(4, 20);
-            this.LabelMatriz1.Name = "LabelMatriz1";
-            this.LabelMatriz1.Size = new System.Drawing.Size(44, 13);
-            this.LabelMatriz1.TabIndex = 4;
-            this.LabelMatriz1.Text = "Matriz 1";
+            this.PanelMatriz1.AutoScroll = true;
+            this.PanelMatriz1.Location = new System.Drawing.Point(6, 16);
+            this.PanelMatriz1.Name = "PanelMatriz1";
+            this.PanelMatriz1.Size = new System.Drawing.Size(292, 229);
+            this.PanelMatriz1.TabIndex = 8;
+            // 
+            // LinhasMatriz1
+            // 
+            this.LinhasMatriz1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LinhasMatriz1.Location = new System.Drawing.Point(54, 17);
+            this.LinhasMatriz1.Name = "LinhasMatriz1";
+            this.LinhasMatriz1.Size = new System.Drawing.Size(40, 20);
+            this.LinhasMatriz1.TabIndex = 8;
+            // 
+            // ColunasMatriz1
+            // 
+            this.ColunasMatriz1.Location = new System.Drawing.Point(100, 17);
+            this.ColunasMatriz1.Name = "ColunasMatriz1";
+            this.ColunasMatriz1.Size = new System.Drawing.Size(32, 20);
+            this.ColunasMatriz1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // groupBoxMatriz2
+            // 
+            this.groupBoxMatriz2.Controls.Add(this.PanelMatriz2);
+            this.groupBoxMatriz2.Location = new System.Drawing.Point(334, 84);
+            this.groupBoxMatriz2.Name = "groupBoxMatriz2";
+            this.groupBoxMatriz2.Size = new System.Drawing.Size(301, 252);
+            this.groupBoxMatriz2.TabIndex = 0;
+            this.groupBoxMatriz2.TabStop = false;
+            this.groupBoxMatriz2.Text = "Matriz 2";
+            // 
+            // PanelMatriz2
+            // 
+            this.PanelMatriz2.AutoScroll = true;
+            this.PanelMatriz2.Location = new System.Drawing.Point(6, 16);
+            this.PanelMatriz2.Name = "PanelMatriz2";
+            this.PanelMatriz2.Size = new System.Drawing.Size(289, 236);
+            this.PanelMatriz2.TabIndex = 8;
+            // 
+            // ButtonCriarMatriz2
+            // 
+            this.ButtonCriarMatriz2.Location = new System.Drawing.Point(465, 17);
+            this.ButtonCriarMatriz2.Name = "ButtonCriarMatriz2";
+            this.ButtonCriarMatriz2.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCriarMatriz2.TabIndex = 3;
+            this.ButtonCriarMatriz2.Text = "Criar Matriz";
+            this.ButtonCriarMatriz2.UseVisualStyleBackColor = true;
+            this.ButtonCriarMatriz2.Click += new System.EventHandler(this.ButtonCriarMatriz1_Click);
+            // 
+            // LabelMatriz2
+            // 
+            this.LabelMatriz2.AutoSize = true;
+            this.LabelMatriz2.Location = new System.Drawing.Point(331, 20);
+            this.LabelMatriz2.Name = "LabelMatriz2";
+            this.LabelMatriz2.Size = new System.Drawing.Size(44, 13);
+            this.LabelMatriz2.TabIndex = 4;
+            this.LabelMatriz2.Text = "Matriz 2";
+            // 
+            // LabelLeiDeFormação2
+            // 
+            this.LabelLeiDeFormação2.AutoSize = true;
+            this.LabelLeiDeFormação2.Location = new System.Drawing.Point(330, 55);
+            this.LabelLeiDeFormação2.Name = "LabelLeiDeFormação2";
+            this.LabelLeiDeFormação2.Size = new System.Drawing.Size(86, 13);
+            this.LabelLeiDeFormação2.TabIndex = 5;
+            this.LabelLeiDeFormação2.Text = "Lei de Formação";
+            // 
+            // ButtonAplicar2
+            // 
+            this.ButtonAplicar2.Location = new System.Drawing.Point(528, 55);
+            this.ButtonAplicar2.Name = "ButtonAplicar2";
+            this.ButtonAplicar2.Size = new System.Drawing.Size(74, 23);
+            this.ButtonAplicar2.TabIndex = 7;
+            this.ButtonAplicar2.Text = "Aplicar";
+            this.ButtonAplicar2.UseVisualStyleBackColor = true;
+            // 
+            // LinhasMatriz2
+            // 
+            this.LinhasMatriz2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LinhasMatriz2.Location = new System.Drawing.Point(381, 17);
+            this.LinhasMatriz2.Name = "LinhasMatriz2";
+            this.LinhasMatriz2.Size = new System.Drawing.Size(40, 20);
+            this.LinhasMatriz2.TabIndex = 8;
+            // 
+            // ColunasMatriz2
+            // 
+            this.ColunasMatriz2.Location = new System.Drawing.Point(427, 17);
+            this.ColunasMatriz2.Name = "ColunasMatriz2";
+            this.ColunasMatriz2.Size = new System.Drawing.Size(32, 20);
+            this.ColunasMatriz2.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(422, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // groupBoxMatrizResultante
+            // 
+            this.groupBoxMatrizResultante.Controls.Add(this.PanelMatrizResultante);
+            this.groupBoxMatrizResultante.Location = new System.Drawing.Point(666, 84);
+            this.groupBoxMatrizResultante.Name = "groupBoxMatrizResultante";
+            this.groupBoxMatrizResultante.Size = new System.Drawing.Size(301, 252);
+            this.groupBoxMatrizResultante.TabIndex = 0;
+            this.groupBoxMatrizResultante.TabStop = false;
+            this.groupBoxMatrizResultante.Text = "Matriz Resultante";
+            // 
+            // PanelMatrizResultante
+            // 
+            this.PanelMatrizResultante.AutoScroll = true;
+            this.PanelMatrizResultante.Location = new System.Drawing.Point(6, 16);
+            this.PanelMatrizResultante.Name = "PanelMatrizResultante";
+            this.PanelMatrizResultante.Size = new System.Drawing.Size(289, 236);
+            this.PanelMatrizResultante.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(828, 464);
+            this.ClientSize = new System.Drawing.Size(993, 464);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -130,6 +294,13 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.groupBoxMatriz1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LinhasMatriz1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColunasMatriz1)).EndInit();
+            this.groupBoxMatriz2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LinhasMatriz2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColunasMatriz2)).EndInit();
+            this.groupBoxMatrizResultante.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -140,10 +311,25 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.GroupBox groupBoxMatriz1;
-        private System.Windows.Forms.TextBox ColunasMatriz1;
-        private System.Windows.Forms.TextBox LinhasMatriz1;
         private System.Windows.Forms.Button ButtonCriarMatriz1;
         private System.Windows.Forms.Label LabelMatriz1;
+        private System.Windows.Forms.Label LabelLeiDeFormação1;
+        private System.Windows.Forms.Button ButtonAplicar1;
+        private System.Windows.Forms.Panel PanelMatriz1;
+        private System.Windows.Forms.NumericUpDown ColunasMatriz1;
+        private System.Windows.Forms.NumericUpDown LinhasMatriz1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown ColunasMatriz2;
+        private System.Windows.Forms.NumericUpDown LinhasMatriz2;
+        private System.Windows.Forms.Button ButtonAplicar2;
+        private System.Windows.Forms.Label LabelLeiDeFormação2;
+        private System.Windows.Forms.Label LabelMatriz2;
+        private System.Windows.Forms.Button ButtonCriarMatriz2;
+        private System.Windows.Forms.GroupBox groupBoxMatriz2;
+        private System.Windows.Forms.Panel PanelMatriz2;
+        private System.Windows.Forms.GroupBox groupBoxMatrizResultante;
+        private System.Windows.Forms.Panel PanelMatrizResultante;
     }
 }
 
--- CalculadoraDeMatrizes/Form1.cs
+++ CalculadoraDeMatrizes/Form1.cs
@@ -12,7 +12,8 @@ namespace CalculadoraDeMatrizes
 {
     public partial class Form1 : Form
     {
-        private TextBox[,] Matriz1;
+        private NumericUpDown[,] Matriz1;
+        private NumericUpDown[,] Matriz2;
 
         public Form1()
         {
@@ -21,25 +22,28 @@ namespace CalculadoraDeMatrizes
 
         private void ButtonCriarMatriz1_Click(object sender, EventArgs e)
         {
-            groupBoxMatriz1.Controls.Clear();
-            int linhas1 = Convert.ToInt32(LinhasMatriz1.Text);
-            int colunas1 = Convert.ToInt32(ColunasMatriz1.Text);
-            Matriz1 = new TextBox[linhas1, colunas1];
-            int TamanhoLinhas = groupBoxMatriz1.Width / colunas1;
-            int TamanhoColunas = groupBoxMatriz1.Height / linhas1;
+            PanelMatriz1.Controls.Clear();
+            PanelMatriz2.Controls.Clear();
+            int i1 = Convert.ToInt32(LinhasMatriz1.Text);
+            int j1 = Convert.ToInt32(ColunasMatriz1.Text);
+            int i2 = Convert.ToInt32(LinhasMatriz2.Text);
+            int j2 = Convert.ToInt32(ColunasMatriz2.Text);
+            Matriz1 = new NumericUpDown[i1, j1];
+            Matriz2 = new NumericUpDown[i2, j2];
+            int TamanhoLinhas = PanelMatriz1.Width / j1;
+            int TamanhoColunas = PanelMatriz1.Height / i1;
             for (int x = 0; x < Matriz1.GetLength(0); x++)
             {
                 for (int y = 0; y < Matriz1.GetLength(1); y++)
                 {
-                    Matriz1[x, y] = new TextBox();
+                    Matriz1[x, y] = new NumericUpDown();
                     Matriz1[x, y].Text = "0";
-                    //Matriz1[x, y].Top = (x * Matriz1[x, y].Height) + 20;
+                    Matriz1[x, y].DecimalPlaces = 1;
+                    Matriz1[x, y].Top = x * Matriz1[x, y].Height;
                     Matriz1[x, y].Left = y * TamanhoLinhas;
-                    Matriz1[x, y].Top = x * TamanhoColunas;
-
                     Matriz1[x, y].Width = TamanhoLinhas;
                     Matriz1[x, y].Height = TamanhoColunas;
-                    groupBoxMatriz1.Controls.Add(Matriz1[x, y]);
+                    PanelMatriz1.Controls.Add(Matriz1[x, y]);
                 }
             }
         }
